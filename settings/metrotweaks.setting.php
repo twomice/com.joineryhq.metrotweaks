20b8981f8b17fd1b2cd612fa9c58d34bdb9a260f
lucky091588 <lucky091588@gmail.com>
2020-12-15 05:41:35 +0800

    Replace ts() with E::ts()

--- settings/metrotweaks.setting.php
+++ settings/metrotweaks.setting.php
@@ -10,10 +10,10 @@ return [
     'html_type' => 'textarea',
     'default' => FALSE,
     'add' => '5.0',
-    'title' => ts('Activity Type Properties'),
+    'title' => E::ts('Activity Type Properties'),
     'is_domain' => 0,
     'is_contact' => 0,
-    'description' => ts('An array of settings for the Metro Tweaks extension.'),
+    'description' => E::ts('An array of settings for the Metro Tweaks extension.'),
     'help_text' => NULL,
   ],
 ];
